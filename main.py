4356fbff06735fed6814204aad74c5afa0d06fe8
SyedSalmanAli2001 <syed.salman.ali@outlook.com>
2024-08-25 14:23:57 +0500

    reset with new array

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
 # Majority Element
-nums = [4,5,4,5,4,5,5,4,4]
+nums = [4,5,4,5,4,5,5,4,4,2,1,9]
 
 
-def majority_element(nums):
-    flag_a = 0
-    flag_b = 0
-    element = 0
-    for i in range(len(nums)):
-        if nums[i] == nums[0]:
-            flag_a += 1
-        if nums[i] != nums[0]:
-            flag_b += 1
-            element = nums[i]
-    if flag_a > len(nums)/2:
-        return nums[0]
-    else:
-        return element
-
-
-print(majority_element(nums))
+def majority_element(array):
