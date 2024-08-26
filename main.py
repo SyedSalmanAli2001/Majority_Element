d3d11bea52a480e5417d6eb40e3328ea0f00f5a9
SyedSalmanAli2001 <syed.salman.ali@outlook.com>
2024-08-26 22:39:27 +0500

    Boyer-Moore Implementation

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-# Majority Element
-nums = [4,5,4,5,4,5,5,4,4,2,1,9]
+# Majority Element - Boyer-Moore Voting Algorithm
+nums_array = [5, 1, 4, 5, 2, 4, 5, 5, 5]
 
 
 def majority_element(array):
+    result, count = 0, 0
+    for i in range(len(array)):
+        if count == 0:
+            result = array[i]
+        if array[i] == result:
+            count += 1
+        elif array[i] != result:
+            count -= 1
+    return result
+
+
+print(majority_element(nums_array))
